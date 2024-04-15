ca6037058c8be1dbd48a6e8b891f205ca6bd631d
nestorraulcg <nestorraulcgomez@gmail.com>
2024-04-15 19:34:22 -0400

    first commit

---
+++ src/test/java/com/example/msclinic/MsClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
